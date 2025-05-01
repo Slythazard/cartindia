1ea11997b7ac97be582423ad2be176606c57da75
Slythazard <shreshthkatyayan25@gmail.com>
2025-05-01 14:42:13 +0530

    changed frontend config

--- client/app/constants/index.js
+++ client/app/constants/index.js
@@ -1,4 +1,5 @@
-export const API_URL = process.env.REACT_APP_API_URL;
+export const API_URL =
+  process.env.REACT_APP_API_URL || 'https://cartindia.onrender.com/ap';
 
 export const SOCKET_URL =
   window.location.hostname === 'localhost'
